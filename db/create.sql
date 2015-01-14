1dd267db786ba668032b29b139cf50c5259c3138
Lookout <446227103@qq.com>
2015-01-14 14:36:22 +0800

    数据库文件增加

--- db/create.sql
+++ db/create.sql
@@ -10,11 +10,8 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- 导出 betterjs 的数据库结构
-DROP DATABASE IF EXISTS `betterjs`;
-CREATE DATABASE IF NOT EXISTS `betterjs` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `betterjs`;
-
+CREATE DATABASE  IF NOT EXISTS `badjs` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `badjs`;
 
 -- 导出  表 betterjs.b_statistics 结构
 DROP TABLE IF EXISTS `b_statistics`;
@@ -28,9 +25,84 @@ CREATE TABLE IF NOT EXISTS `b_statistics` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- 正在导出表  betterjs.b_statistics 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `b_statistics` DISABLE KEYS */;
-/*!40000 ALTER TABLE `b_statistics` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `b_apply`
+--
+
+DROP TABLE IF EXISTS `b_apply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `b_apply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `status` int(11) NOT NULL,
+  `name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `url` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `mail` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  `passTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `b_approve`
+--
+
+DROP TABLE IF EXISTS `b_approve`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `b_approve` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `userName` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `reply` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `applyId` int(11) NOT NULL,
+  `createTime` timestamp NULL DEFAULT NULL,
+  `applyStatus` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `applyId` (`applyId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `b_user`
+--
+
+DROP TABLE IF EXISTS `b_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `b_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `loginName` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `chineseName` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-01-14 12:00:12
+
