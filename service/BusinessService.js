e355343ff9a62ff1a682fbc2a4c35dd013ed293c
Lookout <446227103@qq.com>
2015-01-20 22:24:59 +0800

    增加businessService 接口

--- service/BusinessService.js
+++ service/BusinessService.js
@@ -32,6 +32,26 @@ BusinessService.prototype = {
             callback(null, data);
         });
 
+    },
+    findBusinessByProjectOwner : function (userName , callback){
+
+        var string = "select a.id, u.loginName, u.chineseName, ua.role, a.name "+
+            "from  b_user as u join b_user_apply as ua on(ua.userId = u.id) "+
+            "join b_apply as a on (a.id =ua.applyId) "+
+            "where  a.status=? and u.loginName=?  and ua.role =1";
+        var condition = [Apply.STATUS_PASS ];
+
+        condition.push(userName);
+
+        // console.log(string);
+        this.db.driver.execQuery(string,condition, function (err, data) {
+            if(err){
+                callback(err);
+                return;
+            }
+            callback(null, data);
+        });
+
     }
 
 }
