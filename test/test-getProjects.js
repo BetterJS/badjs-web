a4a8413a719b5380549a78fa88eb93152bfff0f7
Chris Cai <caihuijigood@gmail.com>
2015-03-10 11:01:55 +0800

    修改地址

--- test/test-getProjects.js
+++ test/test-getProjects.js
@@ -1,16 +1,8 @@
 var http = require('http');
 
-var strParams = '';
 
-_.each( item , function (value , ke){
-    strParams+=value.id+"_";
-});
 
-if(strParams.length >0){
-    strParams = "projectsId="+strParams.substring(0 , strParams.length-1   ) + "&";
-}
-
-strParams +="auth=badjsAccepter";
+var strParams="auth=badjsAccepter&projectsId=6_7_911_4_3_5";
 
 
 http.get( "http://10.143.132.205:9001/getProjects" + '?' + strParams , function (res){
