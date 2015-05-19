d7ba2d47b0ed4ab4ac53a10c93d604de99da93ed
Chris Cai <caihuijigood@gmail.com>
2015-05-19 17:41:34 +0800

    修改标题显示

--- controller/action/UserAction.js
+++ controller/action/UserAction.js
@@ -29,7 +29,7 @@ var userAction = {
             });
         }else {
             businessService.findBusinessByProjectOwner(user.loginName , function (err, item){
-                res.render('userManage', { layout: false, user: user, index:'manage', title: '用户列表' , items : item} );
+                res.render('userManage', { layout: false, user: user, index:'manage', manageTitle: '用户列表' , items : item} );
             });
         }
 
@@ -37,7 +37,7 @@ var userAction = {
 
     authUserManger : function (params , req ,  res){
         var user = req.session.user;
-        res.render('authUserManage', { layout: false, user: user, index:'manage', title: '用户授权' } );
+        res.render('authUserManage', { layout: false, user: user, index:'manage', manageTitle: '用户授权' } );
 
     },
 
