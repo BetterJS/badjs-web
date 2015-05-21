29eb65ed3f8cc246035b7a1579fe5fa0584bb887
yorts52 <yorts52@qq.com>
2015-05-21 20:34:35 +0800

    Revert "rename function"
    This reverts commit 3d38f64b815250a91bd37d1d95990902cab3b280.

--- service/EmailService.js
+++ service/EmailService.js
@@ -20,7 +20,7 @@ var EmailService = function() {
     this.from = GLOBAL.pjconfig.email.from || "noreply-badjs@tencent.com";
 };
 
-var getYesterday = function() {
+var getYestoday = function() {
     var date = new Date();
     date.setDate(date.getDate() - 1);
     date.setHours(0, 0, 0, 0);
@@ -66,7 +66,7 @@ EmailService.prototype = {
     },
     queryAll: function(isRetry, times) {
         var that = this;
-        that.date = getYesterday();
+        that.date = getYestoday();
         logger.info('Send mail query all start');
         that.userService.queryListByCondition({
             applyId: -1,
