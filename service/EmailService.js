e49cf7b4aef637f50ddf3ecd459ab73d88a2fcc2
Chris Cai <caihuijigood@gmail.com>
2015-08-28 09:57:56 +0800

    chart put top

--- service/EmailService.js
+++ service/EmailService.js
@@ -102,6 +102,12 @@ EmailService.prototype = {
         that.homepage && html.push('<p style="font-size:12px"><a href="{{homepage}}">日志查看点这: {{homepage}}</a></p>'.replace(/{{homepage}}/g, that.homepage));
         var content = data.content;
         if (content && content.length) {
+
+            if(imagePath){
+                html.push('<h4>最近7天图表统计</h4>')
+                html.push('<p><img src="http://badjs.server.com/'+imagePath+'"></p>');
+            }
+
             html.push('<table style="border-collapse:collapse;;width:95%"><tr style="background-color:#188eee;text-align:left;color:#fff"><th style="padding:2px 0 2px 10px;border:1px solid #dedede;width:60px">#</th><th style="padding:2px 0 2px 10px;border:1px solid #dedede;;width:120px">出现次数</th><th style="padding:2px 0 2px 10px;border:1px solid #dedede">错误内容</th></tr>');
             var total_top = 0;
             content.forEach(function(v, i) {
@@ -124,12 +130,6 @@ EmailService.prototype = {
                 .replace(/{{per}}/g, (total_top * 100 / total).toFixed(2) + '%')
             );
 
-            if(imagePath){
-                html.push("<p>&nbsp;</p>")
-                html.push("<p>&nbsp;</p>")
-                html.push('<h4>最近7天图表统计</h4>')
-                html.push('<p><img src="http://badjs.server.com/'+imagePath+'"></p>');
-            }
 
         } else {
             html.push('<p style="border-top:1px solid #666;margin-top:20px;width:520px;padding:5px 0 0 10px">暂无数据</p>');
