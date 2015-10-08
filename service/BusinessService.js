0584480297d3c1890bcc76d3d74702d6723a35fd
yorts52 <yorts52@qq.com>
2015-10-08 23:06:58 +0800

    add referer check

--- service/BusinessService.js
+++ service/BusinessService.js
@@ -3,30 +3,30 @@
  */
 
 
-var  Apply = require('../model/Apply');
-    _ = require('underscore');
+var Apply = require('../model/Apply');
+var _ = require('underscore');
 
-var BusinessService = function (){
+var BusinessService = function() {
 
     this.db = global.models.db;
-}
+};
 
 
 BusinessService.prototype = {
 
-    findBusinessByUser : function (userName , callback){
+    findBusinessByUser: function(userName, callback) {
 
-        var string = "select a.id, u.loginName, u.chineseName, ua.role, a.name "+
-            "from  b_user as u join b_user_apply as ua on(ua.userId = u.id) "+
-            "join b_apply as a on (a.id =ua.applyId) "+
+        var string = "select a.id, u.loginName, u.chineseName, ua.role, a.name " +
+            "from  b_user as u join b_user_apply as ua on(ua.userId = u.id) " +
+            "join b_apply as a on (a.id =ua.applyId) " +
             "where  a.status=? and u.loginName=? ";
-        var condition = [Apply.STATUS_PASS ];
+        var condition = [Apply.STATUS_PASS];
 
         condition.push(userName);
 
         // console.log(string);
-        this.db.driver.execQuery(string,condition, function (err, data) {
-            if(err){
+        this.db.driver.execQuery(string, condition, function(err, data) {
+            if (err) {
                 callback(err);
                 return;
             }
@@ -34,19 +34,19 @@ BusinessService.prototype = {
         });
 
     },
-    findBusinessByProjectOwner : function (userName , callback){
+    findBusinessByProjectOwner: function(userName, callback) {
 
-        var string = "select a.id, u.loginName, u.chineseName, ua.role, a.name "+
-            "from  b_user as u join b_user_apply as ua on(ua.userId = u.id) "+
-            "join b_apply as a on (a.id =ua.applyId) "+
+        var string = "select a.id, u.loginName, u.chineseName, ua.role, a.name " +
+            "from  b_user as u join b_user_apply as ua on(ua.userId = u.id) " +
+            "join b_apply as a on (a.id =ua.applyId) " +
             "where  a.status=? and u.loginName=?  and ua.role =1";
-        var condition = [Apply.STATUS_PASS ];
+        var condition = [Apply.STATUS_PASS];
 
         condition.push(userName);
 
         // console.log(string);
-        this.db.driver.execQuery(string,condition, function (err, data) {
-            if(err){
+        this.db.driver.execQuery(string, condition, function(err, data) {
+            if (err) {
                 callback(err);
                 return;
             }
@@ -55,28 +55,27 @@ BusinessService.prototype = {
 
     },
 
-    findBusiness : function ( callback){
+    findBusiness: function(callback) {
 
-        var string = "select  a.id , a.appkey, u.loginName, u.chineseName  , a.name  "+
-            "from  b_apply a join b_user u on (a.userName=u.loginName)  "+
-            "where  a.status=?  ";
-        var condition = [Apply.STATUS_PASS ];
+        var string = "select a.id, a.url, a.appkey, u.loginName, u.chineseName, a.name " +
+            "from b_apply a join b_user u on (a.userName=u.loginName) " +
+            "where a.status=?";
+        var condition = [Apply.STATUS_PASS];
 
         // console.log(string);
-        this.db.driver.execQuery(string,condition, function (err, data) {
-            if(err){
-                callback(err);
-                return;
+        this.db.driver.execQuery(string, condition, function(err, data) {
+            if (err) {
+                return callback(err);
             }
 
-            _.each(data , function (value , ke){
+            _.each(data, function(value, ke) {
                 value.role = 1;
-            })
+            });
             callback(null, data);
         });
 
     }
 
-}
+};
 
 module.exports = BusinessService;
--- service/LogService.js
+++ service/LogService.js
@@ -1,3 +1,4 @@
+/* global GLOBAL */
 /**
  * Created by chriscai on 2014/12/16.
  */
@@ -13,105 +14,118 @@ var request = require("request");
 
 
 
-var LogService = function (){
+var LogService = function() {
 
-   /* if(GLOBAL.DEBUG){
-        this.queryUrl = 'http://localhost:9000/query';
-    }else {
-        this.queryUrl = 'http://10.143.132.205:9000/query';
-        this.pushProjectUrl = 'http://10.143.132.205:9001/getProjects';
-    //}*/
+    /*
+        if(GLOBAL.DEBUG){
+            this.queryUrl = 'http://localhost:9000/query';
+        }else {
+            this.queryUrl = 'http://10.143.132.205:9000/query';
+            this.pushProjectUrl = 'http://10.143.132.205:9001/getProjects';
+        }
+    */
 
     this.queryUrl = GLOBAL.pjconfig.storage.queryUrl;
     this.pushProjectUrl = GLOBAL.pjconfig.acceptor.pushProjectUrl;
     this.pushProjectUrl2 = GLOBAL.pjconfig.openapi.pushProjectUrl;
 
-    logger.debug('query url : ' + this.queryUrl)
-//    this.url = 'http://127.0.0.1:9000/query';
-}
+    // this.url = 'http://127.0.0.1:9000/query';
+    logger.debug('query url : ' + this.queryUrl);
+};
 
 
 
 LogService.prototype = {
-    query : function (params , callback){
+    query: function(params, callback) {
 
         var strParams = '';
-        for(var key in params) {
-            if(key == 'index'){
-                strParams += key +'='+ params[key]  + '&';
-            }else {
-                strParams += key +'='+ JSON.stringify(params[key] ) + '&';
-
+        for (var key in params) {
+            if (key == 'index') {
+                strParams += key + '=' + params[key] + '&';
+            } else {
+                strParams += key + '=' + JSON.stringify(params[key]) + '&';
             }
         }
-        strParams +='_=1';
-        logger.debug('query param : ' +strParams );
-        http.get( this.queryUrl + '?'+ strParams , function (res){
+        strParams += '_=1';
+        logger.debug('query param : ' + strParams);
+        http.get(this.queryUrl + '?' + strParams, function(res) {
             var buffer = '';
-           res.on('data' , function (chunk){
-               buffer += chunk.toString();
-           }).on('end' , function (){
-               try{
-                    callback(null , JSON.parse(buffer))
-               }catch(e){
-                   callback(e);
-               }
-           })
+            res.on('data', function(chunk) {
+                buffer += chunk.toString();
+            }).on('end', function() {
+                try {
+                    callback(null, JSON.parse(buffer));
+                } catch (e) {
+                    callback(e);
+                }
+            });
 
-        }).on('error' , function (err){
+        }).on('error', function(err) {
             logger.warn('error :' + err);
-            callback(err)
-        })
+            callback(err);
+        });
     },
-    pushProject : function (callback){
+    pushProject: function(callback) {
         var self = this;
 
-        callback || (callback = function (){});
+        callback || (callback = function() {});
 
-        var businessService   = new BusinessService();
+        var businessService = new BusinessService();
 
-        var push = function (){
+        var push = function() {
 
-            businessService.findBusiness(function (err , item){
+            businessService.findBusiness(function(err, item) {
 
                 var strParams = '';
+                var projectsInfo = {};
 
-                _.each( item , function (value , ke){
-                    strParams+=value.id+"|"+value.appkey+"_";
+                _.each(item, function(value, ke) {
+                    strParams += value.id + "|" + value.appkey + "_";
+                    projectsInfo[value.id] = value;
                 });
 
-                if(strParams.length >0){
-                    strParams = strParams.substring(0 , strParams.length-1  ) ;
+                if (strParams.length > 0) {
+                    strParams = strParams.substring(0, strParams.length - 1);
                 }
 
-                //strParams +="auth=badjsAccepter";
-
-                var result = [0,0]
+                var result = [0, 0];
+                
                 // todo fix ? post is async
-                var resultCall = function (){
-                    if(result[0] < 0 && result[1] <0){
-                        callback(new Error("error"))
-                    }else if(result[0] > 0 && result[1] > 0) {
-                        callback()
+                var resultCall = function() {
+                    if (result[0] < 0 && result[1] < 0) {
+                        callback(new Error("error"));
+                    } else if (result[0] > 0 && result[1] > 0) {
+                        callback();
                     }
-                }
+                };
 
-                request.post(self.pushProjectUrl , {form:{projectsId : strParams, auth : "badjsAccepter"}} , function (err ){
-                    if(err){
+                request.post(self.pushProjectUrl, {
+                    form: {
+                        projectsId: strParams,
+                        projectsInfo: JSON.stringify(projectsInfo),
+                        auth: "badjsAccepter"
+                    }
+                }, function(err) {
+                    if (err) {
                         logger.warn('push projectIds to acceptor  error :' + err);
-                        result[0] = -1
-                    }else {
+                        result[0] = -1;
+                    } else {
                         logger.info('push projectIds to acceptor  success');
                         result[0] = 1;
                     }
                     resultCall();
                 });
 
-                request.post(self.pushProjectUrl2 , {form:{projectsId : strParams , auth : "badjsOpen"}}  , function (err ){
-                    if(err){
+                request.post(self.pushProjectUrl2, {
+                    form: {
+                        projectsId: strParams,
+                        auth: "badjsOpen"
+                    }
+                }, function(err) {
+                    if (err) {
                         logger.warn('push projectIds to open  error :' + err);
                         result[1] = -1;
-                    }else {
+                    } else {
                         logger.info('push projectIds to openapi success');
                         result[1] = 1;
                     }
@@ -119,13 +133,12 @@ LogService.prototype = {
                 });
 
             });
-        }
+        };
 
         push();
 
     }
-}
+};
 
 
-module.exports =  LogService;
-
+module.exports = LogService;
